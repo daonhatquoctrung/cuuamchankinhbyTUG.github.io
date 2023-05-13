dad9b1b95114943b77085be5d6298c21ba2ec72c
daonhatquoctrung <115434806+daonhatquoctrung@users.noreply.github.com>
2023-05-13 19:33:27 +0700

    Add files via upload

---
+++ src.js
@@ -0,0 +1,23 @@
+
+  
+    var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+var i;
+var slides = document.getElementsByClassName("slider")[0].getElementsByTagName("img");
+
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+
+slideIndex++;
+
+if (slideIndex > slides.length) {
+slideIndex = 1
+}
+
+slides[slideIndex - 1].style.display = "block";
+
+setTimeout(showSlides, 2000); 
+}
